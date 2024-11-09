6218e7331748c4ca77f5ccf51032dedfc814e7df
janaajankovic <janajankoviic01@gmail.com>
2024-11-09 12:41:10 +0100

    add user models

---
+++ Backend/src/main/java/com/example/OnlyBuns/model/Administrator.java
@@ -0,0 +1,19 @@
+package com.example.OnlyBuns.model;
+
+/*import com.example.OnlyBuns.enums.UserRole;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name="administrator")
+public class Administrator extends User {
+
+    public Administrator(Integer id, String email, String username, String password, String name, String surname, UserRole userType) {
+        super(id, email, username, password, name, surname, userType);
+    }
+
+    public Administrator() {
+
+    }
+}
+ */
