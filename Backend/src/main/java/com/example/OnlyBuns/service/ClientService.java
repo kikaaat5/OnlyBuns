0eba316875b69c944e71a335a3fe2fd953a36c73
Kristina Todorovic <todorovic.kristina003@gmail.com>
2024-11-11 18:40:35 +0100

    fix: code causing bug commented

--- Backend/src/main/java/com/example/OnlyBuns/service/ClientService.java
+++ Backend/src/main/java/com/example/OnlyBuns/service/ClientService.java
@@ -55,13 +55,13 @@ public class ClientService {
         clientRepository.deleteById(id);
     }
 
-    public List<Client> searchByName(String name) {
+    /*public List<Client> searchByName(String name) {
         return clientRepository.findByNameContainingIgnoreCase(name);
     }
 
     public List<Client> searchBySurname(String surname) {
         return clientRepository.findBySurnameContainingIgnoreCase(surname);
-    }
+    }*/
 
     public List<Client> searchByEmail(String email) {
         return clientRepository.findByEmailContainingIgnoreCase(email);
